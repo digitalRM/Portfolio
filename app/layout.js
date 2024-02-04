3f7b039d78c302296b3b02803f63a79ad3dc941e
Ruslan M <r1554899@gmail.com>
2024-02-04 15:33:05 -0800

    Metadata Update

--- app/layout.js
+++ app/layout.js
@@ -10,10 +10,10 @@ export const viewport = {
 export const metadata = {
   metadataBase: new URL('https://www.ruslan.in'),
   title: 'Ruslan Mukhamedvaleev',
-  description: "Ruslan Mukhamedvaleev is a student and founder of FOYM, dedicated to assisting nonprofits and small businesses with web development. He is also the Director of Front-End Development at 4Human, a Data Science Intern at Washington State University, and involved in various other organizations focused on technology, youth leadership, and community service. He is passionate about technology, particularly coding and web development, and advocates for language access in education as a bilingual immigrant from Kyrgyzstan.",
+  description: "I'm a student and the founder of FOYM, a not-for-profit organization that tries to help nonprofits and small businesses with web development. At 4Human, I'm honored to serve as the Director of Front-End Development, and I'm also a Data Science Intern at Washington State University. I'm fortunate to be part of many different organizations that focus on technology, youth leadership, and community service. I'm also really passionate about language access in education as a bilingual immigrant from Kyrgyzstan.",
   openGraph: {
     title: 'Ruslan Mukhamedvaleev',
-    description: "Ruslan Mukhamedvaleev is a student and founder of FOYM, dedicated to assisting nonprofits and small businesses with web development. He is also the Director of Front-End Development at 4Human, a Data Science Intern at Washington State University, and involved in various other organizations focused on technology, youth leadership, and community service. He is passionate about technology, particularly coding and web development, and advocates for language access in education as a bilingual immigrant from Kyrgyzstan.",
+    description: "I'm a student and the founder of FOYM, a not-for-profit organization that tries to help nonprofits and small businesses with web development. At 4Human, I'm honored to serve as the Director of Front-End Development, and I'm also a Data Science Intern at Washington State University. I'm fortunate to be part of many different organizations that focus on technology, youth leadership, and community service. I'm also really passionate about language access in education as a bilingual immigrant from Kyrgyzstan.",
     url: 'https://www.ruslan.in',
     siteName: 'Ruslan Mukhamedvaleev',
     images: [
