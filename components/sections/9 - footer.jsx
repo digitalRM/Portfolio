a6784b7d44032ee0d73063e30af897ebdd15958e
Ruslan M <r1554899@gmail.com>
2024-02-05 14:13:18 -0800

    Add MIT License to footer

--- components/sections/9 - footer.jsx
+++ components/sections/9 - footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className="mx-auto md:flex md:items-center md:justify-between ">
         <div className='flex flex-col sm:flex-row justify-between w-full'>
           <p className="sm:text-center text-xs font-medium leading-5 text-neutral-900">
-            &copy; 2024 - Ruslan Mukhamedvaleev - All Rights Reserved.
+            &copy; 2024 - Ruslan Mukhamedvaleev - MIT License
           </p>
           <p className="sm:text-center mt-4 sm:mt-0 text-xs font-medium leading-5 text-neutral-900">
             <a href="https://github.com/digitalRM/portfolio" target="_blank" rel="noopener noreferrer" className="text-neutral-900 hover:text-neutral-800 transition-all">View Source Code <ArrowUpRight className='inline-block mb-0.5 -ml-0.5' size={16} /></a>
