b564c3c194742b27699b3f328b6ff97d4836cc2e
Ruslan Mukhamedvaleev <r1554899@gmail.com>
2024-08-12 04:07:58 -0700

    Fix

--- components/sections/2 - aboutMe.jsx
+++ components/sections/2 - aboutMe.jsx
@@ -5,7 +5,7 @@ export default async function AboutMe() {
     <div className="mt-12">
       <h1 className="font-semibold tracking-tight text-black text-xl">About Me</h1>
       <div className="mt-3 border-b border-neutral-200" />
-      <p className="mt-6 text-sm sm:text-base leading-6 text-neutral-800/80">I'm a full-time high school student passionate about design, research, and coding. Over the past year, I've taken on the task of designing and developing several projects. I am deeply passionate about advocacy, especially for language access within education. I'm also an immigrant from Kyrgyzstan and speak fluent Russian and English.</p>
+      <p className="mt-6 text-sm sm:text-base leading-6 text-neutral-800/80">I'm a student passionate about design, research, and coding. Over the past year, I've taken on the task of designing and developing several projects. I am deeply passionate about advocacy, especially for language access within education. I'm also an immigrant from Kyrgyzstan and speak fluent Russian and English.</p>
     </div>
   )
 }
